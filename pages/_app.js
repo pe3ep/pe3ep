8178b00f0d3f8991c1012664baf23709dd2070aa
Andrew <legendarysuwe@gmail.com>
2022-04-08 20:06:22 +0300

    Change main page | Add page loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
 import Navbar from '../layouts/Navbar'
 import Head from 'next/head'
+import ProgressBar from '@badrap/bar-of-progress'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const progress = new ProgressBar({
+  size: 2,
+  color: '#DC2626',
+  className: 'bar-of-progress',
+  delay: 50,
+})
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', progress.start)
+    router.events.on('routeChangeComplete', progress.finish)
+    router.events.on('routeChangeError', progress.finish)
+
+    return () => {
+      router.events.off('routeChangeStart', progress.start)
+      router.events.off('routeChangeComplete', progress.finish)
+      router.events.off('routeChangeError', progress.finish)
+    }
+  }, [router])
+
   return (
     <>
       <Head>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
@@ -8,13 +12,26 @@ export default function Home() {
       <Head>
         <title>Main page</title>
       </Head>
-      <h1>Hello!</h1>
-      {/* <footer className={styles.primaryFooter}>
-        <div>
-          <h3>Made by Andrew Pe3ep</h3>
-          <p>Made with Next JS v12.1.4</p>
+
+      <div className={styles.opener}>
+        <h1>Welcome to my website.</h1>
+        <div className={styles.btnWrapper}>
+          <Link href="/about">
+            <button data-type="primary" className="btn">
+              <FontAwesomeIcon icon={faUser} height={'2.5em'} />
+              About me
+            </button>
+          </Link>
+          <button
+            onClick={() => (window.location = 'https://github.com/pe3ep')}
+            data-type="secondary"
+            className="btn"
+          >
+            <FontAwesomeIcon icon={faGithub} height={'2.5em'} />
+            My GitHub
+          </button>
         </div>
-      </footer> */}
+      </div>
     </>
   )
 }
